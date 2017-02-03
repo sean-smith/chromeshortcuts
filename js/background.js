21c9b85643cf30b908e28f17d55dd521705c6c98
Sean Smith <seanwssmith@gmail.com>
2017-02-02 23:16:06 -0500

    When a man you like switches from what he said a year ago, or four years ago, he is a broad-minded man who has courage enough to change his mind with changing conditions.  When a man you don't like does it, he is a liar who has broken his promises. 		-- Franklin Adams

---
+++ js/background.js
@@ -0,0 +1,88 @@
+/*
+ * Sean Smith
+ * Alias 2016
+*/
+
+var aliases = {};
+
+chrome.storage.sync.get(null, function(obj) {
+  for (o in obj) {
+    aliases[o] = obj[o];
+  }
+});
+
+var re = /[\d\s\+\-=\(\)\*]+/g;
+var link = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
+var search_url = /https:\/\/www\.google\.com\/search\?q=(\w+)&/;
+
+
+// On input changed, call this
+chrome.omnibox.onInputChanged.addListener(
+  function(text, suggest) {
+    var suggestions = [];
+    for (key in aliases) {
+      if (key.startsWith(text) || text == "") {
+        var desc = `<match>${text}: </match><dim>${key} → </dim><url>${aliases[key]}</url>`;
+        suggestions.push({content: aliases[key], description: desc});
+      }
+    }
+    if (text.match(re)) {
+      var result = eval(text).toString();
+      // alert(result);
+      chrome.omnibox.setDefaultSuggestion({description: `<match>= </match><url>${result}</url>`});
+    }
+    else if (suggestions.length > 0) {
+      var first = suggestions.splice(0, 1)[0];
+      chrome.omnibox.setDefaultSuggestion({description: first['description']});
+    }
+    else {
+      chrome.omnibox.setDefaultSuggestion({description: `<match>${text}: </match><dim> - Google Search</dim>`});
+    }
+    suggest(suggestions);
+});
+
+chrome.webRequest.onBeforeRequest.addListener(function(request) {
+  var results = search_url.exec(request.url)[1].trim();
+  if (results in aliases) {
+      return {
+        redirectUrl : aliases[results]
+      };
+  }
+}, {urls : ["<all_urls>"]}, ["blocking"]);
+
+
+
+// Keyboard Shortcut to open Omnibox Alias
+chrome.commands.onCommand.addListener(function(command) {
+  if (command == 'open-omnibox-alias') {
+    console.log('hi');
+  }
+});
+
+// This event is fired with the user accepts the input in the omnibox.
+chrome.omnibox.onInputEntered.addListener(
+  function(text) {
+    if (text in aliases) {
+      chrome.tabs.update({ url: aliases[text] } ); 
+    }
+    else if (text.match(link)) {
+      chrome.tabs.update({ url: text } ); 
+    }
+    else if (text.match(re)) {
+      var result = eval(text).toString();
+      chrome.tabs.update({url: `https://google.com/search?q=${result}`})
+    }
+    else {
+      chrome.tabs.update({url: `https://google.com/search?q=${text}`})
+    }
+});
+
+// Starting input
+chrome.omnibox.onInputStarted.addListener(function() {
+  chrome.storage.sync.get(null, function(obj) {
+    for (o in obj) {
+      aliases[o] = obj[o];
+    }
+  });
+});
+
---
+++ js/popup.js
@@ -0,0 +1,92 @@
+// Copyright Sean Smith 2017
+// Just kidding
+// I'm not a lawyer
+// So copy all you want 🤑
+
+
+// Click handler (jquery because I'm lazy af)
+$(function() {
+
+	get();
+
+	$("#clear").click(function() {
+		clear();
+	});
+
+	$("#current").click(function(event) {
+		chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+			if (tabs[0] != null) {
+				$("#alias").val(tabs[0].title);
+				$("#url").val(tabs[0].url);
+			}
+			$("#alias").select();
+		});
+	});
+
+	$(".url").click(function(event) {
+		// var url = $(this).text();
+		alert("hi");
+		chrome.tabs.update({ url: "https://google.com" } ); 
+	});
+
+	// Form submission handler
+	$("#new_alias").submit(function(event) {
+		var alias = $("#alias").val();
+		var url = $("#url").val();
+
+		if (alias == "" || url == "") {
+			event.preventDefault();
+			alert("You must enter an alias and url...");
+		}
+
+		set(alias, url);
+		return false;
+	});
+
+});
+
+// Store newly input keys
+function set(alias, url) {
+	var obj = {};
+	obj[alias] = url;
+	chrome.storage.sync.set(obj, function() {
+  		$("#alias").val("");
+		$("#url").val("");
+	});
+}
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+    for (key in changes) {
+      var storageChange = changes[key];
+      if (storageChange.newValue != null) {
+      	insert(key, storageChange.newValue);
+      }
+    }
+});
+
+
+// Get keys and display
+function get() {
+	chrome.storage.sync.get(null, function(obj) {
+		for (o in obj) {
+			insert(o, obj[o]);
+		}
+	});
+}
+
+function clear() {
+	chrome.storage.sync.clear(function() {
+		console.log('cleared!')
+	});
+	$("#aliases").html("");
+}
+
+function remove(alias) {
+	chrome.storage.sync.remove(alias, function() {
+		$("#"+alias).remove();
+	});
+}
+
+function insert(alias, url) {
+	$("#aliases").append(`<div class='row'><div class='pill alias'>${alias}</div><img class='icon arrow' src='img/arrow_right.svg'><div class='pill url'>${url}</div></div>`);
+}
