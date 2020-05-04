d0b2a6eb87701ef4e5e3c8c4d96ef26a3e8001e2
Sean Smith <seaam@amazon.com>
2020-05-04 12:59:17 -0700

    Added Ability to Remove a Single Element and Added Confirm Remove
    Signed-off-by: Sean Smith <seaam@amazon.com>

--- js/popup.js
+++ js/popup.js
@@ -9,8 +9,8 @@ $(function() {
 
 	get();
 
-	$("#clear").click(function() {
-		clear();
+	$("#clearall").click(function() {
+		confirm();
 	});
 
 	$("#current").click(function(event) {
@@ -24,7 +24,7 @@ $(function() {
 	});
 
 	$("#github").click(function() {
-		chrome.tabs.update({ url: "https://github.com/sean-smith/chrome_shortcuts" } );
+		chrome.tabs.update({ url: "https://github.com/sean-smith/chromeshortcuts" } );
 		return false; 
 	});
 
@@ -36,6 +36,7 @@ $(function() {
 		if (alias == "" || url == "") {
 			event.preventDefault();
 			alert("You must enter an alias and url...");
+			return;
 		}
 
 		set(alias, url);
@@ -73,6 +74,17 @@ function get() {
 	});
 }
 
+function confirm() {
+	$("#aliases").prepend(`<div id="confirm"><p>Are you sure you want to remove all aliases?</p><button type='button' id='confirmclearall' class='button red'>Yes</button><button type='button' id='cancelclearall' class='button'>No</button></div>`);
+	$("#confirmclearall").click(function(event) {
+		clear();
+		return false;
+	});
+	$("#cancelclearall").click(function(event) {
+		$("#confirm").html("");
+	});
+}
+
 function clear() {
 	chrome.storage.sync.clear(function() {
 		console.log('cleared!')
@@ -82,12 +94,17 @@ function clear() {
 
 function remove(alias) {
 	chrome.storage.sync.remove(alias, function() {
-		$("#"+alias).remove();
+		$("#"+alias).parent().remove();
 	});
 }
 
 function insert(alias, url) {
-	$("#aliases").append(`<div class='row'><div class='pill alias'>${alias}</div><img class='icon arrow' src='img/arrow_right.svg'><div class='pill url'>${url}</div></div>`);
+	$("#aliases").append(`<div class='row'><a class='remove' id="${alias}"><svg class="icon" enable-background="new 0 0 32 32" height="32px" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M20.377,16.519l6.567-6.566c0.962-0.963,0.962-2.539,0-3.502l-0.876-0.875c-0.963-0.964-2.539-0.964-3.501,0  L16,12.142L9.433,5.575c-0.962-0.963-2.538-0.963-3.501,0L5.056,6.45c-0.962,0.963-0.962,2.539,0,3.502l6.566,6.566l-6.566,6.567  c-0.962,0.963-0.962,2.538,0,3.501l0.876,0.876c0.963,0.963,2.539,0.963,3.501,0L16,20.896l6.567,6.566  c0.962,0.963,2.538,0.963,3.501,0l0.876-0.876c0.962-0.963,0.962-2.538,0-3.501L20.377,16.519z" fill="#515151"/></svg></a><div class='pill alias'>${alias}</div><img class='icon arrow' src='img/arrow_right.svg'><div class='pill url'>${url}</div></div>`);
+	$(".remove").click(function(event) {
+		var alias = $(".remove").attr('id');
+		remove(alias);
+		return false;
+	});
 }
 
 var _gaq = _gaq || [];
