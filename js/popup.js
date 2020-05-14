a851dcfd0d60e093e6a922f54df3ef9d1fcd1cd4
Sean Smith <seaam@amazon.com>
2020-05-14 11:42:28 -0700

    Delete Correct Entry
    * Fixes https://github.com/sean-smith/chromeshortcuts/issues/3
    * Also another bugfix for aliases with non-ascii characters

--- js/popup.js
+++ js/popup.js
@@ -35,7 +35,7 @@ $(function() {
 
 	// Form submission handler
 	$("#new_alias").submit(function(event) {
-		var alias = $("#alias").val();
+		var alias = $("#alias").val().replace(/[^\w]/g, '');
 		var url = $("#url").val();
 
 		if (alias == "" || url == "") {
@@ -99,14 +99,14 @@ function clear() {
 
 function remove(alias) {
 	chrome.storage.sync.remove(alias, function() {
-		$("#"+alias).parent().remove();
+		$(`#${alias}`).parent().remove();
 	});
 }
 
 function insert(alias, url) {
 	$("#aliases").append(`<div class='row'><a class='remove' id="${alias}"><svg class="icon" enable-background="new 0 0 32 32" height="32px" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M20.377,16.519l6.567-6.566c0.962-0.963,0.962-2.539,0-3.502l-0.876-0.875c-0.963-0.964-2.539-0.964-3.501,0  L16,12.142L9.433,5.575c-0.962-0.963-2.538-0.963-3.501,0L5.056,6.45c-0.962,0.963-0.962,2.539,0,3.502l6.566,6.566l-6.566,6.567  c-0.962,0.963-0.962,2.538,0,3.501l0.876,0.876c0.963,0.963,2.539,0.963,3.501,0L16,20.896l6.567,6.566  c0.962,0.963,2.538,0.963,3.501,0l0.876-0.876c0.962-0.963,0.962-2.538,0-3.501L20.377,16.519z" fill="#515151"/></svg></a><div class='pill alias'>${alias}</div><img class='icon arrow' src='img/arrow_right.svg'><div class='pill url'>${url}</div></div>`);
-	$(".remove").click(function(event) {
-		var alias = $(".remove").attr('id');
+	$(`#${alias}`).click(function(event) {
+		console.log(alias);
 		remove(alias);
 		return false;
 	});
